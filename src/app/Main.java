d6ebbce7dee59c23d507f235091d503ca00335b8
Oleksandr13012002 <guzoleksandr37@gmail.com>
2025-03-06 09:52:47 +0200

    add discription

---
+++ src/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Проста консольна програма для конвертації миль у кілометри та навпаки");
+
+
+        scanner.close();
+    }
+}
