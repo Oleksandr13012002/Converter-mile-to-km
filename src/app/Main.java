dd942a17e7ee6314c5c45d8eb79141c3f835fd34
Oleksandr13012002 <guzoleksandr37@gmail.com>
2025-03-06 10:16:08 +0200

    add mile to km

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,11 +3,16 @@ package app;
 import java.util.Scanner;
 
 public class Main {
+    private static final double MILES_TO_KM = 1.60934;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Проста консольна програма для конвертації миль у кілометри та навпаки");
 
+        System.out.print("Введіть значення в милях: ");
+        double miles = scanner.nextDouble();
+        System.out.printf("%.2f миль = %.2f км\n", miles, miles * MILES_TO_KM);
 
         scanner.close();
     }
